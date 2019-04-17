16eff84e6a9dd4c4a3b5936237d8f634ec2872a6
Oliver Gomes <ogomesphoto@gmail.com>
2019-04-16 21:36:48 -0400

    handleSubmit and onChange

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 
-export default class Form extends Component {
-  handleSubmit = () => {
-    console.log("object");
+export default class Form extends Component<{}, {}> {
+  state = {
+    currentTask: "",
+    tasks: []
   };
+  handleSubmit = (e: any) => {
+    e.preventDefault();
+    this.setState({
+      currentTask: "",
+      tasks: [...this.state.tasks, this.state.currentTask]
+    });
+  };
+
   render() {
+    console.log(this.state);
     return (
       <div>
         <h1>React Typescript Todo List</h1>
-        <form onSubmit={() => this.handleSubmit()}>
-          <input type="text" placeholder="Add a task" />
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            type="text"
+            placeholder="Add a task"
+            onChange={e =>
+              this.setState({
+                currentTask: e.target.value
+              })
+            }
+          />
           <button type="submit">Add Task</button>
         </form>
       </div>
