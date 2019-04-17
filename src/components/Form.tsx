ec34d79cf4b52fdfafca6147cda7095809ffee4d
Oliver Gomes <ogomesphoto@gmail.com>
2019-04-16 20:37:51 -0400

    basic input form implement

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
 
 export default class Form extends Component {
+  handleSubmit = () => {
+    console.log("object");
+  };
   render() {
-    return <div>Form Components</div>;
+    return (
+      <div>
+        <h1>React Typescript Todo List</h1>
+        <form onSubmit={() => this.handleSubmit()}>
+          <input type="text" placeholder="Add a task" />
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
+    );
   }
 }
